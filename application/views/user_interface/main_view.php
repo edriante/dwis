4aa0293b9a92166d23dd718ecd5835a8da5f074c
lattesrae <chaesiou@gmail.com>
2025-02-11 15:34:57 +0800

    main view

--- application/views/user_interface/main_view.php
+++ application/views/user_interface/main_view.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="icon" href="../assets/img/download (2).png">
-    <link rel="stylesheet" href="../assets/css/style.css">
-    <link rel="stylesheet" href="../assets/css/dashboard.css">
+    <link rel="icon" href="<?= base_url('assets/img/download (2).png'); ?>">
+    <link rel="stylesheet" href="<?= base_url('assets/css/style.css'); ?>">
+    <link rel="stylesheet" href="<?= base_url('assets/css/dashboard.css'); ?>">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css" rel="stylesheet">
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
     <title>Admin</title>
@@ -15,7 +16,7 @@
     <!-- SIDEBAR -->
     <section id="sidebar">
         <a href="<?= site_url('Main_controller/index'); ?>" class="brand">
-            <img src="<?= base_url('assets/img/download (2).png'); ?>" alt="A" class="profile-img"> <!-- Use base_url for the image -->
+            <img src="<?= base_url('assets/img/download (2).png'); ?>" alt="A" class="profile-img">
             <span class="text">dmin</span>
         </a>
         <ul class="side-menu top">
@@ -69,7 +70,7 @@
             <label for="switch-mode" class="switch-mode"></label>
             <a href="#" class="notification"></a>
             <a href="#" class="profile">
-                <img src="../assets/img/logo.png" alt="Profile">
+            <img src="<?= base_url('assets/img/logo.png'); ?>" alt="Profile">
             </a>
         </nav>
 
@@ -140,7 +141,7 @@
     </section>
 
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-    <script src="../assets/js/dashboard.js"></script> <!-- Link to the new JavaScript file -->
+    <script src="../assets/js/dashboard.js"></script> 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
     <script src="../assets/js/manageusers.js"></script> 
 </body>
